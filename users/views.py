0dc6ab717a9349b23a606ca0e4fc64b5a2a437a4
LonelyOrion <lonelyorion@qq.com>
2017-02-09 17:42:39 +0800

    check post parameters

--- users/views.py
+++ users/views.py
@@ -6,4 +6,12 @@ def login(request):
 	return render(request, 'login.html',  {'userName': 'jayz'})
 
 def loginCheck(request):
+	if request.POST.has_key('username'):
+		username = request.POST['username']
+	else:
+		print 'no'
+	if request.POST.has_key('password'):
+		password = request.POST['password']
+	else:
+		print 'no'
 	return render(request, 'loginCheck.html')
