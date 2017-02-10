5075fd98184eb751e410d3271180f1cf51b9dff4
lonelyorion <lonelyorion@qq.com>
2017-02-10 20:37:53 +0800

    加入数据库并且完善了登录和注册功能

--- users/models.py
+++ users/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from mongoengine import *
 # Create your models here.
+
+connect('pyExam')
+
+class Users(Document):
+	username = StringField(max_length = 30, required = True)
+	password = StringField(max_length = 30, required = True)
+	nickname = StringField(max_length = 30, required = True)
--- users/views.py
+++ users/views.py
@@ -1,24 +1,44 @@
 #coding: utf-8
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from models import Users
 
 # Create your views here.
 def login(request):
-	return render(request, 'login.html',  {'userName': 'jayz'})
+	return render(request, 'login.html')
 
 def loginCheck(request):
-	if request.POST.has_key('username'):
+	try:
 		username = request.POST['username']
-	else:
-		raise Http404('错误的访问方式')
-	if request.POST.has_key('password'):
 		password = request.POST['password']
-	else:
-		raise Http404('错误的访问方式')
-	return render(request, 'loginCheck.html')
+	except:
+		return render(request, 'errorInfo.html', {'errorInfo': '错误的访问方式'})
+
+	user = Users.objects(username=username)
+	if len(user) == 0:
+		return render(request, 'errorInfo.html', {'errorInfo': '用户不存在'})
+
+	user = user[0]
+	if password != user.password:
+		return render(request, 'errorInfo.html', {'errorInfo': '密码错误'})
+
+	return render(request, 'loginCheck.html', {'user': user})
 
 def register(request):
 	return render(request, 'register.html')
 
 def registerCheck(request):
+	try:
+		username = request.POST['username']
+		password = request.POST['password']
+		nickname = request.POST['nickname']
+	except:
+		return render(request, 'errorInfo.html', {'errorInfo': '错误的访问方式'})
+
+	user = Users.objects(username=username)
+	if len(user) == 0:
+		user = Users(username = username, password = password, nickname = nickname)
+		user.save()
+	else:
+		return render(request, 'errorInfo.html', {'errorInfo': '用户已存在'})
 	return render(request, 'registerCheck.html')
