0f4fbe33fe6fbc29678ddc95b90240dd325c0ade
lonelyorion <lonelyorion@qq.com>
2017-02-11 11:25:05 +0800

    add showProblems

--- problems/models.py
+++ problems/models.py
@@ -1,5 +1,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from mongoengine import *
 
 # Create your models here.
+
+connect('pyExam')
+
+class Problems(Document):
+	problem = StringField()
+	options = ListField(StringField(), default=list)
+	answer = StringField()
--- problems/views.py
+++ problems/views.py
@@ -1,10 +1,13 @@
 #coding: utf-8
 from django.shortcuts import render
+from models import Problems
 
 # Create your views here.
 
 def showProblems(request):
 	nickname = request.session['nickname']
+	problems = Problems.objects()
+	
 	return render(request, 'showProblems.html', locals())
 
 def addProblems(request):
@@ -12,8 +15,16 @@ def addProblems(request):
 
 def addProblemsCheck(request):
 	try:
-		problem = request.POST['problem']
-		options = request.POST['options']
-		answer = request.POST['answer']
+		rawProblem = request.POST['problem']
+		rawOptions = request.POST['options']
+		rawAnswer = request.POST['answer']
 	except:
 		return render(request, 'info.html', {'info': '提交内容不完整'})
+
+	problem = rawProblem.strip('\r\n')
+	options = rawOptions.split('\n')
+	answer = rawAnswer.strip('\r\n')
+	problem = Problems(problem = problem, options = options, answer = answer)
+	problem.save()
+
+	return render(request, 'showProblems.html')
